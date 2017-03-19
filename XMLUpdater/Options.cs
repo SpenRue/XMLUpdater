f5da0aaa350497746eba16c246eee584893cae83
Spencer <spenrue@gmail.com>
2017-03-18 20:03:10 -0500

    Initial Commit of code
    Added first commit containing the starting code.

---
+++ XMLUpdater/Options.cs
@@ -0,0 +1,43 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLUpdater
+{
+    public class Options
+    {
+        [Option('f', "file", Required = true, HelpText = "The XML file to modify.")]
+        public string FilePath { get; set;}
+
+        [Option('x', "xpath", Required = true, HelpText = "The XPath to select.")]
+        public string XPath { get; set; }
+
+        [Option('r', "regex", Required = false, HelpText = "The regex to select from the xpath value.")]
+        public string Regex { get; set; }
+
+        [Option('g', "group", DefaultValue = 1, Required = false, HelpText = "The group number of the of the regex to replace, or -1 to replace the entire match. Not zero based")]
+        public int Group { get; set; }
+
+        [Option('t', "text", Required = true, HelpText = "The text to replace the selected xpath value with.")]
+        public string Text { get; set; }
+
+        [Option('v', "verbose", DefaultValue = true, Required = false, HelpText = "Print details during execution.")]
+        public bool Verbose { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"FilePath: { FilePath }");
+            sb.AppendLine($"XPath: { XPath }");
+            sb.AppendLine($"Regex: { Regex }");
+            sb.AppendLine($"Group: { Group }");
+            sb.AppendLine($"Text: { Text }");
+            sb.AppendLine($"Verbose: { Verbose }");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ XMLUpdater/Program.cs
@@ -0,0 +1,136 @@
+﻿using CommandLine;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace XMLUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Options options = new Options();
+            if (!Parser.Default.ParseArgumentsStrict(args, options))
+            {
+                Console.WriteLine("XMLUpdater failed to parse arguments.");
+                return;
+            };
+
+            // Take in file path, Validate it.
+            if(!File.Exists(options.FilePath))
+            {
+                Console.WriteLine($"File \"{options.FilePath}\" does not exist");
+                return;
+            }
+
+            // Take in XPath, Validate XPath results.
+            XmlDocument doc = new XmlDocument();
+            try
+            {
+                doc.Load(options.FilePath);
+            }
+            catch(Exception e)
+            {
+                // TODO: Go through the possible exceptions more specifically
+                Console.WriteLine($"Exception occured when trying to read document \"{options.FilePath}\", {Environment.NewLine}{e.Data}");
+                return;
+            }
+
+            XPathNavigator nav = doc.CreateNavigator();
+            XPathNodeIterator iterator = null;
+            try
+            {
+                iterator = nav.Select(options.XPath);
+            }
+            catch (Exception e) when (e is ArgumentException || e is XPathException)
+            {
+                if (e is ArgumentException)
+                {
+                    Console.WriteLine("XPath argument is not valid.");
+                    return;
+                }
+                else if (e is XPathException)
+                {
+                    Console.WriteLine($"Error occured while processing XPath expression, {e.Data}");
+                    return;
+                }
+            }
+            iterator.MoveNext();
+            string value = iterator.Current.Value;
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                Console.WriteLine("The XPath did not yield any results");
+                return;
+            }
+
+            string newValue = null;
+            
+            if(string.IsNullOrWhiteSpace(options.Text))
+            {
+                Console.WriteLine("Text is empty");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(options.Regex))
+            {
+                // Replace whole value, rather than a regex selection of that value.
+                newValue = options.Text;
+            }
+            else
+            {
+                // Replace value based on a regex selection
+                Regex regex = null;
+                Match match = null;
+                try
+                {
+                    regex = new Regex(options.Regex);
+                }
+                catch(ArgumentException e)
+                {
+                    Console.WriteLine($"Regex argument {options.Regex} is invalid. {e.Data}");
+                    return;
+                }
+                
+                try
+                {
+                     match = regex.Match(value);
+                }
+                catch(RegexMatchTimeoutException)
+                {
+                    Console.WriteLine("Regex match timed out");
+                    return;
+                }
+
+                // Determine selection, and replace it with a new value.
+                Group selection;
+                if (options.Group == 0)
+                {
+                    Console.WriteLine("Zero is not valid option for \"group\" argument");
+                    return;
+                }
+                else if(options.Group == -1)
+                    selection = match;
+                else
+                    selection = match.Groups[options.Group];
+
+                string firstPart = value.Substring(0, selection.Index);
+                string secondPart = value.Substring(selection.Index + selection.Length);
+                newValue = firstPart + options.Text + secondPart;
+            }
+
+            Console.WriteLine($"Updating {value} to {newValue}");
+            iterator.Current.SetValue(newValue);
+            
+            try
+            {
+                doc.Save(options.FilePath);
+            }
+            catch(XmlException e)
+            {
+                Console.WriteLine($"Unable to save changes to the xml document. Error: {e.Data}");
+            }
+        }
+    }
+}
